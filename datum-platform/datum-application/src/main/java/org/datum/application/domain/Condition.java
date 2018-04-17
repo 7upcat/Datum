3b76e18f4daf0b264e07573b27885d2c94138a7b
zhengzheming <7upcat@gmail.com>
2018-04-17 20:05:21 +0800

    修订了 logback 配置

--- datum-platform/datum-application/src/main/java/org/datum/application/domain/Condition.java
+++ datum-platform/datum-application/src/main/java/org/datum/application/domain/Condition.java
@@ -10,7 +10,7 @@ import org.datum.expression.dialect.Dialect;
  * 值对象用来描述事实表和维度表之间关联的条件.
  * 
  * @author 7cat
- * @see SqlCondition
+ * @see ExpressionCondition
  * @see CompareCondition
  */
 public interface Condition{
